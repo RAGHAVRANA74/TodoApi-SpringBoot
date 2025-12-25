78eb4722fe5b5c29002bd5835d9cae333fecd12a
Raghav Rana <raghavrana7444@gmail.com>
2025-12-25 08:54:25 +0530

    Added todo creation

--- src/main/java/com/conceptandcoding/learingspringboot/TodoController.java
+++ src/main/java/com/conceptandcoding/learingspringboot/TodoController.java
@@ -1,5 +1,8 @@
 package com.conceptandcoding.learingspringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,16 +13,26 @@ import java.util.List;
 
 public class TodoController {
 
-    private static List<Todo> todos;
+    private static List<Todo> todoList;
 
     public TodoController(){
-        todos = new ArrayList<>();
-        todos.add(new Todo (1,false , "todo1", 1));
-        todos.add(new Todo (2,true  , "todo2", 2));
-
-
+        todoList = new ArrayList<>();
+        todoList.add(new Todo (1,false , "todo 1", 1));
+        todoList.add(new Todo (2,true  , "todo 2", 2));
     }
 
+ @GetMapping("/todos")
+ public List <Todo> getTodos()
+ {
+     return todoList;
+ }
+
+ @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){ //RequestBody tells that the data of  newTodo(just a variable name) is of same type as of the request springboot will create the object itself .
+        todoList.add(newTodo);
+        return newTodo;
+
+ }
 
 
 
